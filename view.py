4ce55a1a418eb659fcc553e0a1fe47530f58fecc
Leonardo Rossi <leonardo.rossi@unipr.it>
2022-01-12 18:01:52 +0100

    view: fix if only we have bboxes
    Signed-off-by: Leonardo Rossi <leonardo.rossi@unipr.it>

--- view.py
+++ view.py
@@ -21,27 +21,33 @@ for img_id in coco.getImgIds():
     image = cv2.imread(image_path)
 
     for ann in anns:
-        segs = ann["segmentation"]
         bbox = np.array(ann["bbox"])
         bbox[2:4] = bbox[0:2] + bbox[2:4]
 
-        segs = [np.array(seg, np.int32).reshape((1, -1, 2))
-                for seg in segs]
+        # add segmentation if exist
+        if 'segmentation' in ann:
+            segs = ann["segmentation"]
+            segs = [np.array(seg, np.int32).reshape((1, -1, 2))
+                    for seg in segs]
 
-        for seg in segs:
-            cv2.drawContours(image, seg, -1, (0, 255, 0), 2)
-        # third aug -1 means draw all contours in 3-D array, Or
-        # for seg in segs: cv2.fillPoly(image, segm, (0,255,0))
+            for seg in segs:
+                cv2.drawContours(image, seg, -1, (0, 255, 0), 2)
+            # third aug -1 means draw all contours in 3-D array, Or
+            # for seg in segs: cv2.fillPoly(image, segm, (0,255,0))
+
+        # add bbox
         cv2.rectangle(image, (int(bbox[0]), int(bbox[1])),
                       (int(bbox[2]), int(bbox[3])), (0, 0, 255), 2)
 
     fname = image_info[0]["file_name"]
 
     outdir = os.path.join(dest_path, str(ann['category_id']))
-    os.makedirs(outdir, exist_ok=True)
 
     filename = os.path.join(outdir, fname)
     print(filename)
+
+    outdir_dir = os.path.dirname(filename)
+    os.makedirs(outdir_dir, exist_ok=True)
 
     cv2.imwrite(filename, image)
     #  break
